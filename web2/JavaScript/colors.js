845f762af114f642d03b4ddccf7f449014b7ac16
taejin_noh <shxowlstjnoh@gmail.com>
2020-01-14 19:52:32 +0900

    [web2 - JS]
    - 34. 파일로 쪼개서 정리 정돈하기
    - js파일에는 script를 포함하면 안됨

---
+++ web2/JavaScript/colors.js
@@ -0,0 +1,36 @@
+var Links = {
+  setColor : function (color) {
+    var i = 0;
+    var alist = document.querySelectorAll('a');
+    while (i < alist.length) {
+      alist[i].style.color = color;
+      i++;
+    }
+  }
+}
+
+var Body = {
+  setColor : function (color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor : function (color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+
+function nightDayHandler(self) {
+  var target = document.querySelector('body');
+
+  if(self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+    Links.setColor('powderblue');
+
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+    Links.setColor('blue');
+  }
+}
